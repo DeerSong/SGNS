31dc1599185cc5c1ffd40785916c0cfb02c86fa1
张鹿颂 <zhanglusongg@gmail.com>
2017-05-20 11:09:50 +0800

    change exp2.py

--- exp2.py
+++ exp2.py
@@ -24,14 +24,14 @@ from functions import *
 model_enwik = Word2vecMF()
 model_enwik.load_matrices(from_file='enwik-200/matrices.npz')
 
-MAX_ITER = 3
+MAX_ITER = 17
 words = ["him", "five", "main"]
 # for i in range(MAX_ITER):
 #     model_enwik.load_CW('enwik-200/PS40iter_fromSVD_dim200_step5e-05_factors', i)
 #     MF = model_enwik.MF(model_enwik.C, model_enwik.W)
 #     print MF
-model_enwik.load_CW('enwik-200/PS10iter_fromSVD_dim200_step5e-05_factors', 3)
-x = datasets_corr(model_enwik, "datasets", "enwik-200/PS10iter_fromSVD_dim200_step5e-05_factors",MAX_ITER,plot_corrs=False)
+model_enwik.load_CW('enwik-200/PS37iter_fromSVD_dim200_step5e-05_factors', 17)
+x = datasets_corr(model_enwik, "datasets", "enwik-200/PS37iter_fromSVD_dim200_step5e-05_factors",MAX_ITER,plot_corrs=False)
 for i in x:
     print i
     print x[i]
