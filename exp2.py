a02e63844241f9658dc9d20523fd19e2d3b9e38b
张鹿颂 <zhanglusongg@gmail.com>
2017-05-24 11:07:15 +0800

    test 250ac

--- exp2.py
+++ exp2.py
@@ -22,16 +22,16 @@ from functions import *
 
 
 model_enwik = Word2vecMF()
-model_enwik.load_matrices(from_file='enwik-200/matrices250aa.npz')
+model_enwik.load_matrices(from_file='enwik-200/matrices250.npz')
 
-MAX_ITER = 15
+MAX_ITER = 8
 words = ["him", "five", "main"]
-# for i in range(MAX_ITER):
-#     model_enwik.load_CW('enwik-200/PS40iter_fromSVD_dim200_step5e-05_factors', i)
-#     MF = model_enwik.MF(model_enwik.C, model_enwik.W)
-#     print MF
-model_enwik.load_CW('enwik-200/250aa', 7)
-x = datasets_corr(model_enwik, "datasets", "enwik-200/250aa",MAX_ITER,plot_corrs=False)
+for i in range(MAX_ITER):
+    model_enwik.load_CW('enwik-200/250ac', i)
+    MF = model_enwik.MF(model_enwik.C, model_enwik.W)
+    print MF
+model_enwik.load_CW('enwik-200/250ac', 7)
+x = datasets_corr(model_enwik, "datasets", "enwik-200/250ac",MAX_ITER,plot_corrs=False)
 for i in x:
     print i
     print x[i]
